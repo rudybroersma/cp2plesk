ca3c78037b727cba2a638920db4e29c6b7c94526
Rudy Broersma <Rudy Broersma@nuc1>
2015-08-17 10:08:36 +0200

    Issie with accounts without mailboxes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /* index.php - cp2plesk */
 
 include("includes/cpanel.class.php");
@@ -34,34 +35,34 @@ if (array_key_exists("reseller", $arguments)) {
 $sp = $plesk->getServicePlans($reseller);
 
 if (array_key_exists("list-serviceplans", $arguments)) {
-  foreach($sp as $plan) {
-    echo $plan['id'] . ": " . $plan['name'] . "\n";
-  };
- 
-  exit;
+    foreach ($sp as $plan) {
+        echo $plan['id'] . ": " . $plan['name'] . "\n";
+    };
+
+    exit;
 }
 
 if (array_key_exists("list-resellers", $arguments)) {
-  echo "login\t\t\tname\t\tcompany\n\n";
-  foreach($plesk->getResellers() as $resellerName) {
-    echo $resellerName['login'] . ":\t\tname: " . $resellerName['pname'] . "\tcompany: " . $resellerName['cname'] . "\n";
-  };
-  exit;
+    echo "login\t\t\tname\t\tcompany\n\n";
+    foreach ($plesk->getResellers() as $resellerName) {
+        echo $resellerName['login'] . ":\t\tname: " . $resellerName['pname'] . "\tcompany: " . $resellerName['cname'] . "\n";
+    };
+    exit;
 }
 
 if (array_key_exists("list-username", $arguments)) {
-  echo $cp->userdata["USER"] . "\n";
-  exit;
+    echo $cp->userdata["USER"] . "\n";
+    exit;
 }
 
 if (array_key_exists("generate-password", $arguments)) {
-  echo $g->generatePassword() . "\n";
-  exit;
+    echo $g->generatePassword() . "\n";
+    exit;
 }
 
 if (array_key_exists("list-email", $arguments)) {
-  echo $acctemail = $cp->userdata["CONTACTEMAIL"] . "\n";
-  exit;
+    echo $acctemail = $cp->userdata["CONTACTEMAIL"] . "\n";
+    exit;
 }
 
 if (array_key_exists("username", $arguments)) {
@@ -79,7 +80,7 @@ if (array_key_exists("password", $arguments)) {
 if (array_key_exists("list-domains", $arguments)) {
     # output a list of all domains in this backup
     echo $cp->mainDomain . "\n";
-    foreach($cp->addOnDomains as $domain) {
+    foreach ($cp->addOnDomains as $domain) {
         echo $domain . "\n";
     }
     exit;
@@ -92,26 +93,28 @@ if (array_key_exists("list-pointers", $arguments)) {
 
 if (array_key_exists("list-aliases", $arguments)) {
     # output a list of all domains in this backup
-    foreach($cp->parkedDomains as $domain) {
+    foreach ($cp->parkedDomains as $domain) {
         echo $domain . "\n";
     }
     exit;
 }
 
 if (array_key_exists("serviceplan", $arguments) && array_key_exists($arguments['serviceplan'], $sp)) {
-  $valid_serviceplan = true;
-  $serviceplan = $arguments['serviceplan'];
-  $serviceplan_name = $sp[$serviceplan]['name'];  
+    $valid_serviceplan = true;
+    $serviceplan = $arguments['serviceplan'];
+    $serviceplan_name = $sp[$serviceplan]['name'];
 } else {
-  echo "Invalid serviceplan given. Please pass the serviceplan number as parameter (eg. php index.php --serviceplan=5): \n\n";
-  exit;
+    echo "Invalid serviceplan given. Please pass the serviceplan number as parameter (eg. php index.php --serviceplan=5): \n\n";
+    exit;
 };
 
 
 $acctemail = $cp->userdata["CONTACTEMAIL"];
 $domain = $cp->mainDomain;
 
-if (strlen(trim($acctemail)) == 0) { $acctemail = $username . "@" . $domain; };
+if (strlen(trim($acctemail)) == 0) {
+    $acctemail = $username . "@" . $domain;
+};
 
 # CREATE CUSTOMER AND SUBSCRIPTION #
 echo "# Control Panel: http://www." . $cp->mainDomain . ":8880\n";
@@ -134,49 +137,47 @@ echo "chmod -R o+x /var/www/vhosts/" . $cp->mainDomain . "/httpdocs/\n";
 echo "echo \"Redirect 301 /webmail http://webmail." . $cp->mainDomain . "/\" > " . $cp->base . "/homedir/public_html/webmail/.htaccess\n";
 echo "cd " . $cp->base . "/homedir/public_html && /usr/bin/lftp -c 'set ftp:ssl-allow false && open ftp://$username:\"$password\"@localhost && cd httpdocs && mirror --no-symlinks -p -R .'\n";
 
-foreach($cp->parkedDomains as $alias => $value) {
+foreach ($cp->parkedDomains as $alias => $value) {
 # Do not use domalias, as we cannot create mail addresses under aliases.
 #    echo "/opt/psa/bin/domalias -c $alias -domain $domain\n";
     echo "/opt/psa/bin/site -c $alias -hosting true -hst_type phys -webspace-name " . $cp->mainDomain . " -www-root httpdocs -seo-redirect none\n";
-    
-    foreach($dns->getDNSChanges($cp->base . "/dnszones/" . $alias . ".db", $cp->userdata['IP']) as $dnschange) {
+
+    foreach ($dns->getDNSChanges($cp->base . "/dnszones/" . $alias . ".db", $cp->userdata['IP']) as $dnschange) {
         echo $dnschange . "\n";
     }
 }
 
-foreach($cp->addOnDomains as $key => $value) {
+foreach ($cp->addOnDomains as $key => $value) {
     $dest = $cp->subDomains[$value];
     $dest = preg_replace("/^public_html\//", "httpdocs/", $dest);
-    
+
     echo "/opt/psa/bin/site -c $key -hosting true -hst_type phys -webspace-name " . $cp->mainDomain . " -www-root $dest -seo-redirect none\n";
     echo "mkdir " . $cp->base . "/" . $dest . "/webmail/\n";
     echo "echo \"Redirect 301 /webmail http://webmail." . $key . "/\" > " . $cp->base . "/" . $dest . "/webmail/.htaccess\n";
     echo "chgrp psaserv /var/www/vhosts/" . $cp->mainDomain . "/" . $dest . "\n";
-    
-    foreach($dns->getDNSChanges($cp->base . "/dnszones/" . $key . ".db", $cp->userdata['IP']) as $dnschange) {
+
+    foreach ($dns->getDNSChanges($cp->base . "/dnszones/" . $key . ".db", $cp->userdata['IP']) as $dnschange) {
         echo $dnschange . "\n";
     }
-    
 };
 
 /* addOnDomains must be ran first, as subdomains will create subs under addondomains */
-foreach($cp->subDomains as $key  => $value) {
+foreach ($cp->subDomains as $key => $value) {
     $split = explode("_", $key);
     $subdomain = $split[0];
     $domain = isset($split[1]) ? $split[1] : "";
     $value = ereg_replace("^public_html/", "httpdocs/", $value);
-    
+
     $createSubdomain = preg_replace("/\.$/", "", $subdomain . "." . $domain);
-    
+
     /* Create real domains instead of subdomains. cPanel allows e-mail accounts within subdomains. Plesk does not. Thus, create domain instead of subdomain */
 #    echo "/opt/psa/bin/subdomain -c " . $subdomain . " -domain " . $domain . " -www-root " . $value . " -php true\n";
     echo "/opt/psa/bin/site -c " . $createSubdomain . " -hosting true -hst_type phys -webspace-name " . $cp->mainDomain . " -www-root " . $value . " -seo-redirect none\n";
     echo "chgrp psaserv /var/www/vhosts/" . $domain . "/" . $value . "\n";
 }
 $mailDomains = array_merge(
-                array_keys($cp->addOnDomains), 
-                array_keys($cp->parkedDomains)
-                );
+        array_keys($cp->addOnDomains), array_keys($cp->parkedDomains)
+);
 
 $mailDomains[] = $cp->mainDomain;
 
@@ -189,17 +190,23 @@ foreach ($cp->mailAccounts as $domain => $value) {
     if ((array_search($domain, $mailDomains)) !== FALSE) { /* array_search can return 0 which matches false if we dont do type checking. Therefor !== IS MANDATORY */
         foreach ($cp->mailAccounts[$domain]["forwards"] as $mailbox => $forward) {
 
-            if (strpos($forward, ":fail") > -1) { $forward = "reject"; };
-            if (strpos($forward, ":fail") > -1) { $forward = "reject"; };
-            if (strpos($forward, "@") == FALSE) { $forward = $forward . "@" . $cp->mainDomain; };
-            
+            if (strpos($forward, ":fail") > -1) {
+                $forward = "reject";
+            };
+            if (strpos($forward, ":fail") > -1) {
+                $forward = "reject";
+            };
+            if (strpos($forward, "@") == FALSE) {
+                $forward = $forward . "@" . $cp->mainDomain;
+            };
+
             if ($mailbox == "*") {
                 echo "/opt/psa/bin/domain_pref -u " . $domain . " -no_usr " . $forward . "\n";
             } else {
                 $forward = preg_replace('/\s+/', '', $forward); // remove all spaces
                 $forward = preg_replace('/,$/', '', $forward); // remove all commas
-                
-                if (!in_array($mailbox, $createdAccounts)) { 
+
+                if (!in_array($mailbox, $createdAccounts)) {
                     echo "/opt/psa/bin/mail -c '" . $mailbox . "' -mailbox false -forwarding true -forwarding-addresses add:" . $forward . "\n";
                     echo "/opt/psa/bin/spamassassin -u '" . $mailbox . "' -status true -hits 5 -action del\n";
                 } else {
@@ -208,22 +215,25 @@ foreach ($cp->mailAccounts as $domain => $value) {
                 array_push($createdAccounts, $mailbox);
             };
         }
-        foreach ($cp->mailAccounts[$domain]["accounts"] as $mailbox => $data) {
-            if (isset($data['crypt'])) {
-                $crypt = $data['crypt'];
-            } else {
-                $crypt = $password;
+
+        if (isset($cp->mailAccounts[$domain]["accounts"])) {
+            foreach ($cp->mailAccounts[$domain]["accounts"] as $mailbox => $data) {
+                if (isset($data['crypt'])) {
+                    $crypt = $data['crypt'];
+                } else {
+                    $crypt = $password;
+                }
+
+                if (!in_array($mailbox . "@" . $domain, $createdAccounts)) {
+                    echo "/opt/psa/bin/mail -c '" . $mailbox . "@" . $domain . "' -mailbox true -passwd '" . $crypt . "' -passwd_type encrypted\n";
+                    echo "/opt/psa/bin/spamassassin -u '" . $mailbox . "@" . $domain . "' -status true -hits 5 -action del\n";
+                    echo "cp -R '" . $cp->base . "/homedir/mail/" . $domain . "/" . $mailbox . "/.' '/var/qmail/mailnames/" . $domain . "/" . $mailbox . "/Maildir/'\n";
+                    echo "chown -R popuser:popuser '/var/qmail/mailnames/" . $domain . "/" . $mailbox . "'\n";
+                } else {
+                    echo "/opt/psa/bin/mail -u '" . $mailbox . "@" . $domain . "' -mailbox true -passwd '" . $crypt . "' -passwd_type encrypted\n";
+                }
+                array_push($createdAccounts, $mailbox . "@" . $domain);
             }
-            
-            if (!in_array($mailbox . "@" . $domain, $createdAccounts)) { 
-                echo "/opt/psa/bin/mail -c '" . $mailbox . "@" . $domain . "' -mailbox true -passwd '" . $crypt . "' -passwd_type encrypted\n";
-                echo "/opt/psa/bin/spamassassin -u '" . $mailbox . "@" . $domain . "' -status true -hits 5 -action del\n";
-                echo "cp -R '" . $cp->base . "/homedir/mail/" . $domain . "/" . $mailbox .  "/.' '/var/qmail/mailnames/" . $domain . "/" . $mailbox . "/Maildir/'\n";
-                echo "chown -R popuser:popuser '/var/qmail/mailnames/" . $domain . "/" . $mailbox . "'\n";
-            } else {
-                echo "/opt/psa/bin/mail -u '" . $mailbox . "@" . $domain . "' -mailbox true -passwd '" . $crypt . "' -passwd_type encrypted\n";
-            }
-            array_push($createdAccounts, $mailbox . "@" . $domain);
         }
     }
 }
@@ -237,7 +247,9 @@ if (isset($cp->databases["MYSQL"])) {
     }
 
     foreach ($cp->databases["MYSQL"]["dbusers"] as $user => $value) {
-        if ($value['pw'] == "") { $value['pw'] = $password; };
+        if ($value['pw'] == "") {
+            $value['pw'] = $password;
+        };
         echo "/opt/psa/bin/database --create-dbuser " . $user . " -domain " . $cp->mainDomain . " -passwd '" . $value['pw'] . "' -type mysql -any-database\n";
         echo "/usr/bin/mysql -uadmin -p`cat /etc/psa/.psa.shadow` mysql -e \"UPDATE mysql.user SET Password = '" . $value['pw'] . "' WHERE User = '" . $user . "'\"\n";
         echo "/usr/bin/mysql -uadmin -p`cat /etc/psa/.psa.shadow` mysql -e \"FLUSH PRIVILEGES\"\n";
@@ -245,7 +257,7 @@ if (isset($cp->databases["MYSQL"])) {
 }
 
 // Perform DNS changes for main domain
-foreach($dns->getDNSChanges($cp->base . "/dnszones/" . $cp->mainDomain . ".db", $cp->userdata['IP']) as $dnschange) {
+foreach ($dns->getDNSChanges($cp->base . "/dnszones/" . $cp->mainDomain . ".db", $cp->userdata['IP']) as $dnschange) {
     echo $dnschange . "\n";
 }
 
