3924baae24ba0f0409c5385b42adf57c60549187
Rudy Broersma <r.broersma@exsilia.net>
2019-06-25 13:42:45 +0200

    Change mysql to mysqli for PHP7 compatibility.

--- includes/plesk.class.php
+++ includes/plesk.class.php
@@ -13,13 +13,13 @@ class Plesk {
     }
     
     public function isValidReseller($login) {
-        $plesk_db = mysql_connect("localhost", $this->getUsername(), $this->getPassword());
-        mysql_select_db("psa", $plesk_db);
+        $plesk_db = mysqli_connect("localhost", $this->getUsername(), $this->getPassword());
+        mysqli_select_db("psa", $plesk_db);
 
-        $plesk_query = "SELECT * FROM clients WHERE type = 'reseller' and login = '" . mysql_real_escape_string($login) . "'";
-        $plesk_result = mysql_query($plesk_query);
+        $plesk_query = "SELECT * FROM clients WHERE type = 'reseller' and login = '" . mysqli_real_escape_string($login) . "'";
+        $plesk_result = mysqli_query($plesk_query);
         
-        if (mysql_num_rows($plesk_result) == 1) {
+        if (mysqli_num_rows($plesk_result) == 1) {
             return TRUE;
         } else {
             return FALSE;
@@ -29,14 +29,14 @@ class Plesk {
     public function getResellers() {
         $resellers = [];
         
-        $plesk_db = mysql_connect("localhost", $this->getUsername(), $this->getPassword());
-        mysql_select_db("psa", $plesk_db);
+        $plesk_db = mysqli_connect("localhost", $this->getUsername(), $this->getPassword());
+        mysqli_select_db("psa", $plesk_db);
 
         $plesk_query = "SELECT * FROM clients WHERE type = 'reseller'";
         
-        $plesk_result = mysql_query($plesk_query);
+        $plesk_result = mysqli_query($plesk_query);
 
-        while($row = mysql_fetch_assoc($plesk_result)) {
+        while($row = mysqli_fetch_assoc($plesk_result)) {
           $resellers[$row['login']] = $row;
         }
 
@@ -44,19 +44,20 @@ class Plesk {
     }
     
     public function getServicePlans($reseller = FALSE) {
-        $plesk_db = mysql_connect("localhost", $this->getUsername(), $this->getPassword());
-        mysql_select_db("psa", $plesk_db);
+        $plesk_db = mysqli_connect("localhost", $this->getUsername(), $this->getPassword());
+        #mysqli_select_db("psa", $plesk_db);
+        mysqli_select_db($plesk_db, "psa");
 
         if ($reseller == FALSE) {
           $plesk_query = "SELECT id, name FROM Templates WHERE type = 'domain' AND owner_id = 1 AND name != 'Admin Simple'";
         } else {
-          $plesk_query = "SELECT id, name FROM Templates WHERE type = 'domain' AND owner_id = (SELECT id FROM clients WHERE type = 'reseller' AND login = '" . mysql_real_escape_string($reseller) . "')";
+          $plesk_query = "SELECT id, name FROM Templates WHERE type = 'domain' AND owner_id = (SELECT id FROM clients WHERE type = 'reseller' AND login = '" . mysqli_real_escape_string($reseller) . "')";
         }
         
-        $plesk_result = mysql_query($plesk_query);
+        $plesk_result = mysqli_query($plesk_db, $plesk_query);
 
         $sp = array();
-        while($row = mysql_fetch_assoc($plesk_result)) {
+        while($row = mysqli_fetch_assoc($plesk_result)) {
           $sp[$row['id']] = $row;
         }
 
